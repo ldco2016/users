40ed47b40d47226ed3877bbdd981f964379f8b58
danale <danale@danales-MacBook-Pro.local>
2018-06-27 17:16:58 -0400

    Adding subdocumento an existing record

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -2,13 +2,33 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Subdocuments', () => {
-  it('can create a subdocument', (done) => {
+  it('can create a subdocument', done => {
     const joe = new User({ name: 'Joe', posts: [{ title: 'Titulo' }] });
     joe
       .save()
       .then(() => User.findOne({ name: 'Joe' }))
-      .then(user => {
+      .then((user) => {
         assert(user.posts[0].title === 'Titulo');
+        done();
+      });
+  });
+
+  it('Can add subdocuments to an existing record', done => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post' });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
         done();
       });
   });
